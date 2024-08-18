a5ed0513a88506ada41ff16b8c4d6b04c1a41ef6
SARATHCHANDRA I T P it22346872 <it22346872@my.sliit.lk>
2024-08-18 19:06:22 +0530

    util

--- Lab/src/util/UtilC.java
+++ Lab/src/util/UtilC.java
@@ -2,19 +2,20 @@ package util;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.io.IOException;
+
 import java.util.Properties;
 
 
 public class UtilC {
 
 	public static final Properties p = new Properties();
+	private static final Logger logger = Logger.getLogger(UtilC.class.getName());
 
 	static {
 		try {
 			p.load(UtilQ.class.getResourceAsStream("../config/config.properties"));
 		} catch (Exception e) {
-			
+			logger.log(Level.SEVERE, e.toString());
 		}
 	}
 }
